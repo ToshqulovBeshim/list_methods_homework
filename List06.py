51c80c88eab4a210fb9a520c4feb9680250b2515
ToshqulovBeshim <tbeshimboyy@gmail.com>
2023-10-10 11:14:45 +0300

    list06

--- List06.py
+++ List06.py
@@ -1,4 +1,4 @@
-def main(fruits):
+def main(f):
     """
     Given a list called Fruits, it contains at least one apple. Find how many apples are on the list and return.
     Args:
@@ -6,4 +6,6 @@ def main(fruits):
     Returns:
         int: return answer
     """
-    return
+   
+    return f.count("asd")
+print(main( f=["asd","asd","ASDF","asdfghgfds","asdfg"]))
