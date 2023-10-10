f82848fec3045fd78145746b0dec9dc468fa898f
ToshqulovBeshim <tbeshimboyy@gmail.com>
2023-10-10 11:16:27 +0300

    list09

--- List09.py
+++ List09.py
@@ -1,4 +1,4 @@
-def main(fruits):
+def main(f):
     """
     A list called “fruits” is given and is five in length and contains at least one “apple”. Calculate how many “apple” were involved and return the indexes in a list view.
     Args:
@@ -6,4 +6,13 @@ def main(fruits):
     Returns:
         list: return answer
     """
-    return
+    x=f.count("apple")
+    a=0
+    ls=[]
+    while a<len(f):
+        if f[a]=="apple":
+            ls.append(a)
+        a+=1
+    ls.insert(0,x)
+    return ls
+print(main(f = ["apple", "banana", "apple", "pear", "apple"]))
