b988c8910256a0468c749e49083b5ff921a80f96
ToshqulovBeshim <tbeshimboyy@gmail.com>
2023-10-10 11:15:49 +0300

    list08

--- List08.py
+++ List08.py
@@ -1,4 +1,4 @@
-def main(fruits):
+def main(f):
     """
     A list called "fruits" is given and is five in length and contains at least one "apple". Remove the apples from the list and return the list.
     Args:
@@ -6,4 +6,12 @@ def main(fruits):
     Returns:
         list: return answer
     """
-    return
+    a=0
+    ls=[]
+    while a<len(f):
+        if f[a]!="apple":
+            ls.append(f[a])
+        a+=1
+    return ls
+print(main(f=["apple","apple","banana","bear"]))
+print(main(["apple", "apple", "apple", "apple", "kiwi"]))
